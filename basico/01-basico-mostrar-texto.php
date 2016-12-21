3ef9e7bf8d142be182ebff7ff879ff18d79529a9
Oscar Abad Folgueira <oscar@dinapyme.com>
2016-12-21 18:37:51 +0100

    Ejemplos básicos

---
+++ basico/01-basico-mostrar-texto.php
@@ -0,0 +1,7 @@
+<?php
+	// Ejemplo básico de PHP - htts://www.dinapyme.com
+	// Mostrar texto con PHP
+	// En este ejemplo mostramos un texto por pantalla utlizando sólo php, sin html. Utilizamos ECHO.
+	
+	echo "Este texto se muestra....";
+?>
