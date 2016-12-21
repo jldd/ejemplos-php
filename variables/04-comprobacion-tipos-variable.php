7bd4342a5e081087fc682300595db140305d11c3
Oscar Abad Folgueira <oscar@dinapyme.com>
2016-12-21 10:04:43 +0100

    Añadidos tipos double y bool

--- variables/04-comprobacion-tipos-variable.php
+++ variables/04-comprobacion-tipos-variable.php
@@ -12,9 +12,31 @@
 	}
 	echo "<br />";
 	
+	// Comprobación que el contenido de la variable es de tipo Integer (entero).
 	$variable1 = 5;  // Asignar un número entero a $variable1
 	if (is_int($variable1)) {  // Comprobar si es un número entero
-		echo "La variable \$variable1 es de tipo entero";
+		echo "La variable \$variable1 es de tipo Integer.";
+	}
+	echo "<br />";
+	
+	//Comprobación que el contenido de la variable es de tipo String (caneda - texto).
+	$variable1 = "Una cadena de texto";  // Asignar un string a la  variable
+	if(is_string($variable1)) {  // Comprobar si es un string
+		echo "La variable \$variable1 es de tipo String.";
+	}
+	echo "<br />";
+	
+	// Comprobación que el contenido de la variable es de tipo Double.
+	$variable1 = 3.564;  // Asignar un tipo doble a la variable.
+	if(is_double($variable1)) {  // Comprobar si es de tipo Double
+		echo "La variable \$variable1 es de tipo Double.";
+	}
+	echo "<br />";
+	
+	// Comprobación que el contenido de la variable es de tipo Bool.
+	$variable1 = true;  // Asignar un valor de tipo bool a la variable.
+	if(is_bool($variable1)) {  // Comprobar si es de tipo bool
+		echo "La variable \$variable1 es de tipo Bool.";
 	}
 	echo "<br />";
 	
