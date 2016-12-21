c7ea168125905cb0a9f97f2f41f8c50e3714c39c
Oscar Abad Folgueira <oscar@dinapyme.com>
2016-12-21 17:36:37 +0100

    if compuesto or

--- control-de-flujo/if/05-if-expresion-compuesta-or.php
+++ control-de-flujo/if/05-if-expresion-compuesta-or.php
@@ -3,7 +3,7 @@
 	// Ejemplo sobre IF	sin utilizar Else
 	// Si $dia es igual a Sabado o Domingo, mostrar un mensaje.
 	
-	$dia = "Lunes";  // Asignar un día de la semana a la variable $dia
+	$dia = "Sabado";  // Asignar un día de la semana a la variable $dia
 	
 	if (($dia == "Sabado") || ($dia == "Domingo")){   // si el contenido de $edad es mayor o igual a 18 mostrará y menor o igual que 65.
 		echo "Hoy es " . $dia . ". Por lo tanto es Fin de Semana!";
