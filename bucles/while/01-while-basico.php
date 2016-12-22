50e5380bb0b071ec1939838645873a3612c87b3d
Oscar Abad Folgueira <oscar@dinapyme.com>
2016-12-22 13:44:19 +0100

    While básico

---
+++ bucles/while/01-while-basico.php
@@ -0,0 +1,15 @@
+<?php
+	// Ejemplo de Condicionales en  PHP - https://www.dinapyme.com
+	// Ejemplo sobre Operador While
+	// Creamos un buclie con While hasta que el número sea 10.
+	// Dentro del white incrementamos el valor del contador en 1. Se muestra el valor de $cotador en cada iteración.
+	
+	$contador = 1;  // Valor inicial para la variable $contador
+	
+	while ($contador <= 10) {  // Mientras $contador sea menor o igual que 10
+		echo $contador . "<br />";
+		$contador++;
+	}
+	
+	
+?>
